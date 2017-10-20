363025a3f4f87738b3192fb043f5390b1d4ed430
Scepheo <scepheo@gmail.com>
2017-10-20 23:25:22 +0200

    Fix up repository layout

---
+++ The Museum/Assets/Scripts/Attachable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Attachable : MonoBehaviour
+{
+    public Material AttachedMaterial;
+    public Material UnattachedMaterial;
+
+    public bool IsAttached { get; private set; }
+
+    private Renderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void Attach()
+    {
+        IsAttached = true;
+        _renderer.material = AttachedMaterial;
+    }
+
+    public void Detach()
+    {
+        IsAttached = false;
+        _renderer.material = UnattachedMaterial;
+    }
+
+    public bool IsVisible
+    {
+        get { return _renderer.isVisible; }
+    }
+}
---
+++ The Museum/Assets/Scripts/UndoStack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public class UndoStack
+{
+    private Stack<GameState> gameStates;
+    private GameState startState;
+
+    public UndoStack()
+    {
+        gameStates = new Stack<GameState>();
+        startState = GameState.Make();
+        gameStates.Push(startState);
+    }
+
+    /// <summary>
+    /// Pops the last gamestate and applies it.
+    /// </summary>
+    public void Undo()
+    {
+        if (gameStates.Count > 0)
+        {
+            var state = gameStates.Pop();
+            state.Apply();
+        }
+    }
+
+    /// <summary>
+    /// Pushes the current gamestate onto the stack. Should be called before any movement.
+    /// </summary>
+    public void Do()
+    {
+        var newGameState = GameState.Make();
+        gameStates.Push(newGameState);
+    }
+}
